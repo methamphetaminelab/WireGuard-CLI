1cf43f26d97c68345df6fa05442522dc7fc2f0dd
methamphetaminelab <58376312+methamphetaminelab@users.noreply.github.com>
2024-08-13 17:43:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,22 +12,22 @@ echo "Installing dependencies..."
 pip install -r requirements.txt
 venv/bin/pip install -r requirements.txt    
 
-# Проверка наличия cliguard.py
-if [ ! -f "cliguard.py" ]; then
-    echo "File cliguard.py not found!"
+# Проверка наличия tuiguard.py
+if [ ! -f "tuiguard.py" ]; then
+    echo "File tuiguard.py not found!"
     exit 1
 fi
 
 # Изменение прав доступа
-echo "Changing access rights to cliguard.py..."
-chmod +x cliguard.py
+echo "Changing access rights to tuiguard.py..."
+chmod +x tuiguard.py
 
 # Перемещение файла
-echo "Moving cliguard.py to /usr/local/bin/cliguard..."
-sudo cp cliguard.py /usr/local/bin/cliguard
+echo "Moving tuiguard.py to /usr/local/bin/tuiguard..."
+sudo cp tuiguard.py /usr/local/bin/tuiguard
 
 echo "-----------------------------------       -"
 echo "Installation completed successfully!"
-echo "Usage example: sudo cliguard --config(c) wg0 "
-echo "Usage example: sudo cliguard        "
+echo "Usage example: sudo tuiguard --config(c) wg0 "
+echo "Usage example: sudo tuiguard        "
 echo "------------------------------------"
